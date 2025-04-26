fd50b534ec33e25f40ef6b4dfb7e90639445e9c4
Skyzreal <jadefauvelle@hotmail.ca>
2025-04-26 17:54:24 -0400

    Fixing the standalone components

--- press-pause/src/app/interests/user-interests/user-interests.component.ts
+++ press-pause/src/app/interests/user-interests/user-interests.component.ts
@@ -1,15 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { firstValueFrom } from 'rxjs';
 @Component({
   selector: 'app-user-interests',
   templateUrl: './user-interests.component.html',
-  styleUrls: ['./user-interests.component.css']
+  styleUrls: ['./user-interests.component.css'],
+  standalone: true,
+  imports: [CommonModule]
 })
 export class UserInterestsComponent {
-
   constructor(private http: HttpClient) {}
-
   async saveInterests() {
     const userData = {
       movieGenres: this.getCheckedValues('movie-genres'),
@@ -26,7 +27,7 @@ export class UserInterestsComponent {
     console.log('Collected Data:', userData); 
 
     try {
-      const response = await this.http.post('http://localhost:3000/api/user-interest', userData).toPromise();
+      const response = await firstValueFrom(this.http.post('http://localhost:3000/api/user-interest', userData));
       console.log('Server response:', response);
     } catch (error) {
       console.error('Error saving interests:', error);
