c2bf6a395132662da1269e0a89205c652737edcc
Julia Forbes <forbes.juliam@gmail.com>
2025-04-26 18:07:41 -0400

    Finished styling for HTML and loading component added, along with images and headers

--- press-pause/src/app/app.routes.ts
+++ press-pause/src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { UserInterestsComponent } from './interests/user-interests/user-interests.component';
 import { PackagesComponent } from './analog/packages/packages.component';
+import { LoadingComponent } from './loading/loading.component';
 
 export const routes: Routes = [
     { path: 'home', component: AppComponent },
     { path: 'interests', component: UserInterestsComponent },
-    { path: 'packages', component: PackagesComponent }
+    { path: 'packages', component: PackagesComponent },
+    { path: 'loading', component: LoadingComponent}
 ];
