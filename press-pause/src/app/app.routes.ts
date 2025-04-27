92e99224b6ba39e74837b00e6c0c0e2457b011af
Sabrina T. <sabrina.tochkov@gmail.com>
2025-04-27 09:17:28 -0400

    Protected pages with AuthGuard

--- press-pause/src/app/app.routes.ts
+++ press-pause/src/app/app.routes.ts
@@ -5,14 +5,15 @@ import { PackagesComponent } from './analog/packages/packages.component';
 import { LoadingComponent } from './loading/loading.component';
 import { HomeComponent } from './home/home.component';
 import { DevicesComponent } from './analog/devices/devices.component';
+import { AuthGuard } from '@auth0/auth0-angular';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'home', pathMatch: 'full' }, // Default route
     { path: 'home', component: HomeComponent },
-    { path: 'interests', component: UserInterestsComponent },
-    { path: 'packages', component: PackagesComponent },
-    { path: 'loading', component: LoadingComponent},
-    {path: 'devices', component: DevicesComponent},
+    { path: 'interests', component: UserInterestsComponent,  canActivate: [AuthGuard] },
+    { path: 'packages', component: PackagesComponent, canActivate: [AuthGuard] },
+    { path: 'loading', component: LoadingComponent, canActivate: [AuthGuard]},
+    {path: 'devices', component: DevicesComponent, canActivate: [AuthGuard]},
     { path: '**', redirectTo: 'home' } // Wildcard route for 404
 
 ];
